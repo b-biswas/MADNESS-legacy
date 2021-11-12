a69493d784b7ce41c2027e4121626c1e56f75ead
Bbiswas Bbiswas <bbiswas@cca003.in2p3.fr>
2021-11-12 11:54:54 +0100

    fixed bug in loss function

--- trainer.py
+++ trainer.py
@@ -6,16 +6,17 @@ import os
 
 bands = [4,5,6,7,8,9]
 batch_size = 50
-linear_norm = True
-vae_epochs = 100
+linear_norm = False
+vae_epochs = 20
+latent_dim = 32
 
-f_net = FlowVAEnet(linear_norm=linear_norm)
+f_net = FlowVAEnet(latent_dim=latent_dim, linear_norm=linear_norm)
 
 # Keras Callbacks
-path_weights = '/sps/lsst/users/bbiswas/weights/LSST/FlowDeblender/' + 'trial_run/'
+path_weights = '/sps/lsst/users/bbiswas/weights/LSST/FlowDeblender/' + 'latent_32_vars/'
 
 #checkpointer_mse = tf.keras.callbacks.ModelCheckpoint(filepath=path_weights+'mse/weights_noisy_v4.{epoch:02d}-{val_mean_squared_error:.2f}.ckpt', monitor='val_mean_squared_error', verbose=1, save_best_only=True,save_weights_only=True, mode='min', period=1)
-checkpointer_fvae_loss = tf.keras.callbacks.ModelCheckpoint(filepath=path_weights + "fvae/" + 'weights_noisy_v4.{epoch:02d}-{val_loss:.2f}.ckpt', monitor='val_loss', verbose=1, save_best_only=True,save_weights_only=True, mode='min', period=1)
+#checkpointer_fvae_loss = tf.keras.callbacks.ModelCheckpoint(filepath=path_weights + "fvae/" + 'weights_noisy_v4.{epoch:02d}-{val_loss:.2f}.ckpt', monitor='val_loss', verbose=1, save_best_only=True,save_weights_only=True, mode='min', period=1)
 #checkpointer_flow_loss = tf.keras.callbacks.ModelCheckpoint(filepath=path_weights+"full_model/"+'weights_noisy_v4.{epoch:02d}-{val_flow_loss:.2f}.ckpt', monitor='val_flow_loss', verbose=1, save_best_only=True,save_weights_only=True, mode='min', period=1)
 
 terminate_on_nan = tf.keras.callbacks.TerminateOnNaN()
@@ -50,5 +51,5 @@ validation_generator = BatchGenerator(bands, list_of_samples_val, total_sample_s
                                     list_of_weights_e = None,
                                     num_iter_per_epoch=None)
 
-f_net.train_fvae(training_generator, validation_generator, callbacks=[checkpointer_fvae_loss, terminate_on_nan], epochs=vae_epochs)
+f_net.train_fvae(training_generator, validation_generator, epochs=vae_epochs, path_weights=path_weights)
 #f_net.load_weights(weights_path='/sps/lsst/users/bbiswas/weights/LSST/FlowDeblender/trial_run')
