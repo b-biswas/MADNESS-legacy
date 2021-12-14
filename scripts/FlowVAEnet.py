69c309943ca4582bb2019aca3f84f35819838904
b-biswas <bbiswas.dgp@gmail.com>
2021-12-14 14:12:21 +0100

    updated to debvader architecture

--- scripts/FlowVAEnet.py
+++ scripts/FlowVAEnet.py
@@ -1,65 +1,55 @@
-from scripts.model import flow, vae_model
+from scripts.model import create_model_fvae
 import tensorflow as tf
 import tensorflow_probability as tfp
 
 from tensorflow.keras.layers import Input
+import tensorflow.keras.backend as K
 
 tfd = tfp.distributions
 tfb = tfp.bijectors
 
-def vae_loss_fn(x, output):
-    x_decoded_mean = output
-    mse_loss = tf.reduce_sum(tf.keras.metrics.mean_squared_error(x, x_decoded_mean), axis=[1,2]) # TODO: prevent avg over the last dimension multiply suitably
-    tf.print(mse_loss.shape)
-    return mse_loss*6
+@tf.function
+def vae_loss_fn(x, x_decoded_mean):
+    return -x_decoded_mean.log_prob(x)
 
+@tf.function
 def flow_loss_fn(x, output):
-    tf.print(tf.shape(output))
+    #tf.print(tf.shape(output))
     return -output
 
-def kl_loss(x, encoder_output):
-    z_mu = encoder_output[1]
-    z_sig = encoder_output[2]
-    dist_z = tfp.distributions.MultivariateNormalDiag(z_mu, z_sig)
-    dist_0 = tfp.distributions.MultivariateNormalDiag(tf.zeros_like(z_mu), tf.ones_like(z_sig))
-    kl_divergence = tfp.distributions.kl_divergence(dist_z, dist_0, name='KL_divergence_full_cov')
-
-    return kl_divergence
-
 class FlowVAEnet:
-    def __init__(self, latent_dim=32, hidden_dim=256, filters=[32, 64, 128, 256], kernels=[3,3,3,3],nb_of_bands=6, conv_activation=None, dense_activation=None, num_nf_layers=5, linear_norm=True):
 
+    def __init__(self,
+        input_shape = (64, 64, 6),
+        latent_dim = 32,
+        filters = [32,64,128,256],
+        kernels = [3,3,3,3],
+        conv_activation=None,
+        dense_activation=None,
+        linear_norm=False,
+        num_nf_layers=5,
+    ):
+
+        self.input_shape = input_shape
         self.latent_dim = latent_dim
-        self.hidden_dim = hidden_dim
+
         self.filters = filters
         self.kernels = kernels
-        self.nb_of_bands = nb_of_bands
+        self.nb_of_bands = input_shape[2]
         self.conv_activation = conv_activation
         self.dense_activation = dense_activation
         self.num_nf_layers = num_nf_layers
         self.linear_norm = linear_norm
 
-        self.encoder, self.decoder, self.td, self.model = self.build_vae_model()
+        self.model, self.encoder, self.decoder, self.td = create_model_fvae(input_shape=self.input_shape, latent_dim=self.latent_dim, filters=self.filters, kernels=self.kernels, conv_activation=self.conv_activation, dense_activation=self.dense_activation, linear_norm=self.linear_norm)
 
         self.optimizer = None
         self.callbacks = None
 
+    # Custom metric to display the KL divergence during training
+    def kl_metric(y_true, y_pred):
+        return K.sum(self.model.losses)
 
-    def build_vae_model(self):
-
-        encoder, decoder = vae_model(latent_dim=self.latent_dim, hidden_dim=self.hidden_dim, filters=self.filters, kernels=self.kernels, nb_of_bands=self.nb_of_bands, conv_activation=self.conv_activation, dense_activation=self.dense_activation, linear_norm=self.linear_norm)
-        
-        input_vae = Input(shape=encoder.input.shape[1:])
-
-        z, mu, sig = encoder(input_vae)
-
-        td = flow(latent_dim=self.latent_dim, num_nf_layers=self.num_nf_layers)
-        log_prob = td(z)
-        
-        model = tf.keras.Model(input_vae, outputs=[log_prob, decoder(z), mu, sig])
-        
-        return encoder, decoder, td, model
-       
     def train_vae(self, train_generator, validation_generator, path_weights, callbacks, optimizer=tf.keras.optimizers.Adam(1e-6, clipvalue=1), epochs = 35, verbose=1):
         self.td.trainable=False
         self.encoder.trainable=True
@@ -67,7 +57,7 @@ class FlowVAEnet:
         self.model.summary()
         print("Training only VAE network")
         terminate_on_nan = [tf.keras.callbacks.TerminateOnNaN()]
-        self.model.compile(optimizer=optimizer, loss={'decoder': vae_loss_fn, 'encoder': kl_loss})
+        self.model.compile(optimizer=optimizer, loss={'decoder': vae_loss_fn})
         self.model.fit_generator(generator=train_generator, epochs=epochs,
                   verbose=verbose,
                   shuffle=True,
@@ -79,10 +69,10 @@ class FlowVAEnet:
     def train_flow(self, train_generator, validation_generator, path_weights, callbacks, optimizer=tf.keras.optimizers.Adam(1e-6, clipvalue=1), epochs = 35, verbose=1):
         print("Training only Flow net")
         self.td.trainable = True
-        self.encoder.trainable=True
-        self.decoder.trainable=True
+        self.encoder.trainable = True
+        self.decoder.trainable = True
         self.model.summary()
-        self.model.compile(optimizer=optimizer, loss={'decoder': vae_loss_fn, 'flow': flow_loss_fn, 'encoder': kl_loss})
+        self.model.compile(optimizer=optimizer, loss={'flow': flow_loss_fn, 'decoder': vae_loss_fn})
         #self.model.compile(optimizer=optimizer, loss={'flow': flow_loss_fn})
         terminate_on_nan = [tf.keras.callbacks.TerminateOnNaN()]
         self.model.fit_generator(generator=train_generator, epochs=epochs,
@@ -91,7 +81,7 @@ class FlowVAEnet:
                   validation_data=validation_generator,
                   callbacks=callbacks + terminate_on_nan,
                   workers=0, 
-                  use_multiprocessing = True)
+                  use_multiprocessing=True)
 
     def load_weights(self, weights_path, Folder=True):
         if Folder:
--- scripts/model.py
+++ scripts/model.py
@@ -13,104 +13,118 @@ from tensorflow.keras.layers import Conv2D, Input, Dense, Dropout, MaxPool2D, Fl
 tfd = tfp.distributions
 tfb = tfp.bijectors
 
-def build_encoder(latent_dim=32, hidden_dim=256, filters=[32, 64, 128, 256], kernels=[3,3,3,3],nb_of_bands=6, conv_activation=None, dense_activation=None):#'sofplus'
-    """
-    Return encoder as model
-    latent_dim : dimension of the latent variable
-    hidden_dim : dimension of the dense hidden layer
-    filters: list of the sizes of the filters used for this model
-    list of the size of the kernels used for each filter of this model
-    conv_activation: type of activation layer used after the convolutional layers
-    dense_activation: type of activation layer used after the dense layers
-    nb_of bands : nb of band-pass filters needed in the model
-    """
-    input_layer = Input(shape=(64,64,nb_of_bands))
+def create_encoder(
+    input_shape,
+    latent_dim,
+    filters,
+    kernels,
+    conv_activation=None,
+    dense_activation=None,
+):
+    # Define the prior for the latent space
+    prior = tfd.Independent(
+        tfd.Normal(loc=tf.zeros(latent_dim), scale=1), reinterpreted_batch_ndims=1
+    )
 
-    h = Reshape((64,64,nb_of_bands))(input_layer)
-    h = BatchNormalization()(h)
+    # Input layer
+    input_layer = Input(shape=(input_shape))
+
+    # Define the model
+    h = BatchNormalization()(input_layer)
     for i in range(len(filters)):
-        h = Conv2D(filters[i], (kernels[i],kernels[i]), activation=conv_activation, padding='same')(h)
+        h = Conv2D(
+            filters[i], (kernels[i], kernels[i]), activation=None, padding="same"
+        )(h)
         h = PReLU()(h)
-        h = Conv2D(filters[i], (kernels[i],kernels[i]), activation=conv_activation, padding='same', strides=(2,2))(h)
+        h = Conv2D(
+            filters[i],
+            (kernels[i], kernels[i]),
+            activation=None,
+            padding="same",
+            strides=(2, 2),
+        )(h)
         h = PReLU()(h)
+
     h = Flatten()(h)
-    h = Dense(hidden_dim, activation=dense_activation)(h)
     h = PReLU()(h)
-    mu = Dense(latent_dim)(h)
-    sig = Dense(latent_dim, activation='softplus')(h)
+    h = Dense(
+        tfp.layers.MultivariateNormalTriL.params_size(latent_dim), activation=None
+    )(h)
 
-    latent_dist = tfd.MultivariateNormalDiag(mu, sig)
+    # Define the prior for the latent space
+    prior = tfd.Independent(
+        tfd.Normal(loc=tf.zeros(latent_dim), scale=1), reinterpreted_batch_ndims=1
+    )
 
-    z = latent_dist.sample()
-    return Model(input_layer, [z, mu, sig], name ='encoder')
+    h = tfp.layers.MultivariateNormalTriL(
+        latent_dim,
+        activity_regularizer=tfp.layers.KLDivergenceRegularizer(prior, weight=0.01),
+        )(h)
 
-#### Create encooder
-def build_decoder(input_shape, latent_dim=32, hidden_dim=256, filters=[32, 64, 128, 256], kernels=[3,3,3,3], conv_activation=None, dense_activation=None, linear_norm=False):
-    """
-    Return decoder as model
-    input_shape: shape of the input data
-    latent_dim : dimension of the latent variable
-    hidden_dim : dimension of the dense hidden layer
-    filters: list of the sizes of the filters used for this model
-    list of the size of the kernels used for each filter of this model
-    conv_activation: type of activation layer used after the convolutional layers
-    dense_activation: type of activation layer used after the dense layers
-    """
+    return Model(input_layer, [h.sample(), h], name='encoder')
+
+
+def create_decoder(
+    input_shape,
+    latent_dim,
+    filters,
+    kernels,
+    conv_activation=None,
+    dense_activation=None,
+    linear_norm=False,
+):
+
     input_layer = Input(shape=(latent_dim,))
-    h = Dense(hidden_dim, activation=dense_activation)(input_layer)
+    h = PReLU()(input_layer)
+    w = int(np.ceil(input_shape[0] / 2 ** (len(filters))))
+    h = Dense(w * w * filters[-1], activation=dense_activation)(tf.cast(h, tf.float32))
     h = PReLU()(h)
-    w = int(np.ceil(input_shape[0]/2**(len(filters))))
-    h = Dense(w*w*filters[-1], activation=dense_activation)(h)
-    h = PReLU()(h)
-    h = Reshape((w,w,filters[-1]))(h)
-    for i in range(len(filters)-1,-1,-1):
-        h = Conv2DTranspose(filters[i], (kernels[i],kernels[i]), activation=conv_activation, padding='same', strides=(2,2))(h)
+    h = Reshape((w, w, filters[-1]))(h)
+    for i in range(len(filters) - 1, -1, -1):
+        h = Conv2DTranspose(
+            filters[i],
+            (kernels[i], kernels[i]),
+            activation=conv_activation,
+            padding="same",
+            strides=(2, 2),
+        )(h)
         h = PReLU()(h)
-        h = Conv2DTranspose(filters[i], (kernels[i],kernels[i]), activation=conv_activation, padding='same')(h)
+        h = Conv2DTranspose(
+            filters[i],
+            (kernels[i], kernels[i]),
+            activation=conv_activation,
+            padding="same",
+        )(h)
         h = PReLU()(h)
+    
     if linear_norm:
-        h = Conv2D(input_shape[-1], (3,3), activation='relu', padding='same')(h)
+        h = Conv2D(input_shape[-1]*2, (3, 3), activation="relu", padding="same")(h)
     else:
-        h = Conv2D(input_shape[-1], (3,3), activation='sigmoid', padding='same')(h)
-    cropping = int(h.get_shape()[1]-input_shape[0])
-    if cropping>0:
-        print('in cropping')
+        h = Conv2D(input_shape[-1]*2, (3, 3), activation="sigmoid", padding="same")(h)
+
+    # In case the last convolutional layer does not provide an image of the size of the input image, cropp it.
+    cropping = int(h.get_shape()[1] - input_shape[0])
+    if cropping > 0:
+        print("in cropping")
         if cropping % 2 == 0:
-            h = Cropping2D(cropping/2)(h)
+            h = Cropping2D(cropping / 2)(h)
         else:
-            h = Cropping2D(((cropping//2,cropping//2+1),(cropping//2,cropping//2+1)))(h)
+            h = Cropping2D(
+                ((cropping // 2, cropping // 2 + 1), (cropping // 2, cropping // 2 + 1))
+            )(h)
 
+    # Build the encoder only
+    h = tfp.layers.DistributionLambda(make_distribution_fn=lambda t: tfd.Normal(loc=t[...,:input_shape[-1]], scale=1e-4 +t[...,input_shape[-1]:])
+                                          ,convert_to_tensor_fn=tfp.distributions.Distribution.sample)(h)
+    
+    
+    
     return Model(input_layer, h, name='decoder')
-
-
-# Function to define model
-
-def vae_model(latent_dim, hidden_dim, filters, kernels, nb_of_bands, conv_activation=None, dense_activation=None, linear_norm = True):
-    """
-    Function to create VAE model
-    nb_of bands : nb of band-pass filters needed in the model
-    """
-
-    #### Parameters to fix
-    # input_shape: shape of the input data
-    # latent_dim : dimension of the latent variable
-    # hidden_dim : dimension of the dense hidden layer
-    # filters: list of the sizes of the filters used for this model
-    # kernels: list of the size of the kernels used for each filter of this model
-    
-    input_shape = (64, 64, nb_of_bands)
-
-    # Build the encoder
-    encoder = build_encoder(latent_dim, hidden_dim, filters, kernels, nb_of_bands,conv_activation='relu')
-    # Build the decoder
-    decoder = build_decoder(input_shape, latent_dim, hidden_dim, filters, kernels, linear_norm=linear_norm, conv_activation='relu', dense_activation=None)
-    
-    return encoder, decoder
 
 def init_permutation_once(x, name):
     return tf.Variable(name=name, initial_value=x, trainable=False)
 
-def flow(latent_dim=32, num_nf_layers=5):
+def create_flow(latent_dim=32, num_nf_layers=5):
     
     my_bijects = []
     zdist = tfd.MultivariateNormalDiag(loc=[0.0] * latent_dim)
@@ -138,3 +152,53 @@ def flow(latent_dim=32, num_nf_layers=5):
 
     input_layer = Input(shape=(latent_dim))
     return Model(input_layer, td.log_prob(input_layer), name='flow')
+
+# Function to define model
+
+def create_model_fvae(
+    input_shape,
+    latent_dim,
+    filters,
+    kernels,
+    conv_activation=None,
+    dense_activation=None,
+    linear_norm=False,
+):
+    """
+    Create the VAE model
+    parameters:
+        input_shape: shape of input tensor
+        latent_dim: size of the latent space
+        filters: filters used for the convolutional layers
+        kernels: kernels used for the convolutional layers
+    """
+    
+    encoder = create_encoder(
+    input_shape,
+    latent_dim,
+    filters,
+    kernels,
+    conv_activation=None,
+    dense_activation=None,
+)
+    
+    decoder = create_decoder(
+    input_shape,
+    latent_dim,
+    filters,
+    kernels,
+    conv_activation=None,
+    dense_activation=None,
+    linear_norm=linear_norm,
+)
+
+    flow = create_flow(latent_dim=latent_dim, num_nf_layers=5)
+    
+
+    # Build the model
+    x_input = Input(shape=(input_shape), name='input_galaxies')
+    z = encoder(x_input)
+
+    net = Model(inputs = x_input, outputs = [decoder(z[0]), flow(z[0])])
+    
+    return net, encoder, decoder, flow
