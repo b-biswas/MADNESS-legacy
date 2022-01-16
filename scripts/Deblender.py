1e01e02fcfc0d665bd4bb63d462bcf5055d87a83
b-biswas <bbiswas.dgp@gmail.com>
2022-01-16 07:15:19 +0100

    added documentation

--- scripts/Deblender.py
+++ scripts/Deblender.py
@@ -7,10 +7,29 @@ tfd = tfp.distributions
 
 class Deblend:
 
-    def __init__(self, postage_stamp, num_components = 1, max_iter = 100, lr = .1, initW = None, use_likelihood=True, channel_last=False):
+    def __init__(self, postage_stamp, num_components = 1, max_iter = 100, lr = .15, initZ=None, use_likelihood=True, channel_last=False):
+        """
+        
+        Parameters
+        __________
+
+        postage_stamp: np.ndarray
+            input stamp/field that is to be deblended
+        num_components: int
+            number of galaxies present in the image.
+        max_iter: int
+            number of iterations in the deblending step
+        lr: float
+            learning rate for the gradient descent in the latent space.
+        initZ: np.ndarry
+            initial value for the latent space
+        use_likelihood: bool
+            decides whether or not to use the log_prob output of the flow deblender in the optimization. 
+        channel_last: bool
+            if channel is the last column of the postage_stamp
+        """
 
         self.postage_stamp = postage_stamp
-        print(np.shape(postage_stamp))
         self.max_iter = max_iter 
         self.lr = lr 
         self.num_components = num_components 
@@ -23,16 +42,24 @@ class Deblend:
         self.flow_vae_net.load_vae_weights('/sps/lsst/users/bbiswas/weights/LSST/FlowDeblender/separated_architecture/vae/')
         self.flow_vae_net.load_flow_weights('/sps/lsst/users/bbiswas/weights/LSST/FlowDeblender/separated_architecture/fvae/')
 
-        self.flow_vae_net.vae_model.trainable = False
-        self.flow_vae_net.flow_model.trainable = False
+        #self.flow_vae_net.vae_model.trainable = False
+        #self.flow_vae_net.flow_model.trainable = False
 
         self.flow_vae_net.vae_model.summary()
-        self.gradient_decent()
+        self.gradient_decent(initZ)
 
 
     def gradient_decent(self, optimizer=None, initZ=None):
+        """
+        perform the gradient descent step
 
-
+        Parameters
+        ----------
+        optimizer: tf.keras.optimizers
+            optimizer to be used fo rhte gradient descent
+        initZ: np.ndarray
+            initial value of the latent space.
+        """
         X = self.postage_stamp
         if not self.channel_last:
             X = np.transpose(X, axes = (1,2,0))
@@ -48,9 +75,10 @@ class Deblend:
 
         else:
 
-            z = tf.Variable(name="z", initial_value=tf.random_normal_initializer(mean=0, stddev=.01)(shape=[32], dtype=tf.float32))
+            z = tf.Variable(name="z", initial_value=tf.random_normal_initializer(mean=0, stddev=1)(shape=[32], dtype=tf.float32))
+            # TODO: re-train the encoder to find a good starting point.
             #_, initZ = self.flow_vae_net.vae_model(np.expand_dims(X, 0))
-            #z = tf.Variable(initial_value = initZ, shape=tf.TensorShape((1,32)), name ='z')
+            # z = tf.Variable(initial_value = initZ, shape=tf.TensorShape((1,32)), name ='z')
 
         zdist = tfd.MultivariateNormalDiag(loc=[0.0] * 32)
 
@@ -73,14 +101,11 @@ class Deblend:
                     loss = reconstruction_loss
 
             #print(tf.shape(tf.math.reduce_sum(W, axis=0)))
-            #print(log_likelihood)
-            #print(loss)
+            print("sigma :" + str(sig.numpy()))
+            print("log prob flow:" + str(log_likelihood.numpy()))
+            print(loss)
             grad = tape.gradient(loss, [z])
-            
             grads_and_vars=[(grad, [z])]
-
-            #print(W)
-
             optimizer.apply_gradients(zip(grad, [z]))
 
         self.components = reconstruction.numpy()
