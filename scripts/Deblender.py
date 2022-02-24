92547dc9e415709b6399619ff8403ccc43ee0a80
b-biswas <bbiswas.dgp@gmail.com>
2022-02-24 11:00:26 +0100

    first implementation simultaneous deblending

--- scripts/Deblender.py
+++ scripts/Deblender.py
@@ -1,13 +1,15 @@
 import numpy as np
 import tensorflow as tf
 from scripts.FlowVAEnet import FlowVAEnet
+from scripts.extraction import extract_cutouts
 import tensorflow_probability as tfp
+
 
 tfd = tfp.distributions
 
 class Deblend:
 
-    def __init__(self, postage_stamp, detected_positions, cutout_size=64, num_components=1, max_iter=150, lr= .05, initZ=None, use_likelihood=True, channel_last=False):
+    def __init__(self, postage_stamp, detected_positions, cutout_size=64, num_components=1, max_iter=200, lr= .2, initZ=None, use_likelihood=True, channel_last=False):
         """
         Parameters
         __________
@@ -50,7 +52,9 @@ class Deblend:
         #self.flow_vae_net.vae_model.summary()
         self.gradient_decent(initZ)
 
-    def compute_residual(self, reconstructions):
+    def compute_residual(self, reconstructions=None):
+        if reconstructions is None:
+            reconstructions = self.components
         if self.channel_last:
             residual_field = self.postage_stamp.copy()
         else:
@@ -66,43 +70,16 @@ class Deblend:
             #cutout prediction 
             reconstruction = reconstructions[i]
             
-            starting_pos_y = detected_position[0] - (self.cutout_size-1)/2
-            ending_pos_y = detected_position[0] + (self.cutout_size-1)/2
+            starting_pos_x = round(detected_position[0] - (self.cutout_size-1)/2)
+            starting_pos_y = round(detected_position[1] - (self.cutout_size-1)/2)
 
-            starting_pos_x = detected_position[1] - (self.cutout_size-1)/2
-            ending_pos_x = detected_position[1] + (self.cutout_size-1)/2
+            indices = np.indices((self.cutout_size, self.cutout_size, tf.shape(reconstruction)[2])).reshape(3, -1).T
+            indices[:, 0] += int(starting_pos_y)
+            indices[:, 1] += int(starting_pos_x)
 
-            residual_field[int(starting_pos_x) : int(ending_pos_x) + 1, int(starting_pos_y) : int(ending_pos_y) + 1] = tf.subtract(residual_field[int(starting_pos_x) : int(ending_pos_x) + 1, int(starting_pos_y) : int(ending_pos_y) + 1], reconstruction)
+            residual_field = tf.tensor_scatter_nd_sub(residual_field, indices, tf.reshape(reconstruction, -1))
 
         return residual_field
-
-    def predicted_field(self, reconstructions=None):
-        if reconstructions is None:
-            reconstructions = self.num_components
-        
-        prediction = np.zeros_like(self.postage_stamp)
-
-        if not self.channel_last:
-            prediction = np.transpose(prediction, axes=(1,2,0))
-        
-        for i in range(self.num_components):
-            detected_position = self.detected_positions[i]
-
-            #TODO: make this optional
-
-            #cutout prediction 
-            reconstruction = reconstructions[i]
-            max_loc = tf.math.argmax(reconstruction).numpy()
-            
-            starting_pos_x = detected_position[0] - (self.cutout_size-1)/2
-            ending_pos_x = detected_position[0] + (self.cutout_size-1)/2
-
-            starting_pos_y = detected_position[1] - (self.cutout_size-1)/2
-            ending_pos_y = detected_position[1] + (self.cutout_size-1)/2
-
-            prediction[int(starting_pos_x) : int(ending_pos_x) + 1, int(starting_pos_y) : int(ending_pos_y) + 1] += prediction
-
-        return prediction
 
     def gradient_decent(self, optimizer=None, initZ=None):
         """
@@ -132,10 +109,11 @@ class Deblend:
 
             z = tf.Variable(name="z", initial_value=tf.random_normal_initializer(mean=0, stddev=1)(shape=[self.num_components, 32], dtype=tf.float32))
             # TODO: re-train the encoder to find a good starting point.
-            #_, initZ = self.flow_vae_net.vae_model(np.expand_dims(X, 0))
+            distances_to_center = list(np.array(self.detected_positions) - self.cutout_size/2)
+            cutouts = extract_cutouts(X, m, distances_to_center, cutout_size=self.cutout_size, nb_of_bands=b)
+            initZ = self.flow_vae_net.encoder(cutouts)
+            initZ = initZ
             # z = tf.Variable(initial_value = initZ, shape=tf.TensorShape((1,32)), name ='z')
-
-        zdist = tfd.MultivariateNormalDiag(loc=[0.0] * 32)
 
         optimizer = tf.keras.optimizers.Adam(lr = self.lr)
 
@@ -150,9 +128,9 @@ class Deblend:
 
                 residual_field = self.compute_residual(reconstructions)
 
-                reconstruction_loss = tf.math.reduce_sum(tf.square(residual_field)) / tf.cast(tf.square(sig), tf.float64)
+                reconstruction_loss = tf.cast(tf.math.reduce_sum(tf.square(residual_field)), tf.float32) / tf.cast(tf.square(sig), tf.float32)
 
-                log_likelihood = tf.cast(tf.math.reduce_sum(self.flow_vae_net.flow(tf.reshape(z,(self.num_components, 32)))), tf.float64)
+                log_likelihood = tf.cast(tf.math.reduce_sum(self.flow_vae_net.flow(tf.reshape(z,(self.num_components, 32)))), tf.float32)
                 if self.use_likelihood:
                     loss = tf.math.subtract(reconstruction_loss, log_likelihood)
                 else:
