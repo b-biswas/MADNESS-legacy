505da0e7ddda3fe5ad760bf07129770917ffd14e
b-biswas <bbiswas.dgp@gmail.com>
2022-03-04 15:08:46 +0100

    train the model in 3 steps

--- train_models.py
+++ train_models.py
@@ -6,11 +6,11 @@ from scripts.utils import listdir_fullpath
 import os
 
 # define the parameters
-batch_size = 200 
+batch_size = 200
 linear_norm = True
-generative_epochs = 20
-flow_epochs = 20
-deblender_epochs =20
+generative_epochs = 100
+flow_epochs = 100
+deblender_epochs = 100
 latent_dim = 32
 num_iter_per_epoch = None
 
@@ -31,47 +31,44 @@ train_path = datalist[:800]
 validation_path = datalist[800:]
 
 
+
+# Step 1: learn latent space representation of the Galaxies.
 ######## Define the generators
 train_generator = COSMOSsequence(train_path, 'blended_gal_stamps', 'isolated_gal_stamps', 
-                                 batch_size=200, num_iterations_per_epoch=400,
+                                 batch_size=batch_size, num_iterations_per_epoch=400,
                                  normalization_func=linear_normalization_cosmos, use_only_isolated=True)
 
 validation_generator = COSMOSsequence(validation_path, 'blended_gal_stamps', 'isolated_gal_stamps', 
-                                 batch_size=200, num_iterations_per_epoch=100, 
+                                 batch_size=batch_size, num_iterations_per_epoch=100, 
                                  normalization_func=linear_normalization_cosmos, use_only_isolated=True)
-
-
-# Step 1: learn latent space representation of the Galaxies.
-
 ######## Define all used callbacks
 checkpointer_vae_loss = tf.keras.callbacks.ModelCheckpoint(filepath=path_weights + "vae/" + 'weights_isolated.{epoch:02d}-{val_loss:.2f}.ckpt', monitor='val_loss', verbose=1, save_best_only=True,save_weights_only=True, mode='min', period=1)
 # now train the model
-f_net.train_vae(train_generator, validation_generator, callbacks=[checkpointer_vae_loss], epochs=generative_epochs, path_weights=path_weights)
+f_net.train_vae(train_generator, validation_generator, callbacks=[checkpointer_vae_loss], epochs=generative_epochs)
+
 
 
 # Step 2: optimize the log prob using the flow network
-
+######## Define the generators
+train_generator.num_iterations_per_epoch=100
+validation_generator.num_iterations_per_epoch=100
+# load the vae weights for encoder 
+#f_net.load_vae_weights("/pbs/throng/lsst/users/bbiswas/train_debvader/cosmos/weights/deblender/val_loss")
 ######## Define all used callbacks
 checkpointer_flow_loss = tf.keras.callbacks.ModelCheckpoint(filepath=path_weights + "fvae/" + 'weights_isolated.{epoch:02d}-{val_loss:.2f}.ckpt', monitor='val_loss', verbose=1, save_best_only=True,save_weights_only=True, mode='min', period=1)
-
 # now train the model
-f_net.train_flow(train_generator, validation_generator, callbacks= [checkpointer_flow_loss], epochs=flow_epochs, path_weights=path_weights)
+f_net.train_flow(train_generator, validation_generator, callbacks= [checkpointer_flow_loss], epochs=flow_epochs)
+
 
 
 # Step 3: to find  a good initial guess for the deblender, train the encoder as a deblender.
-
 ######## Define the generators
 # To deblend, feed blended galaies as input to the model
-train_generator = COSMOSsequence(train_path, 'blended_gal_stamps', 'isolated_gal_stamps', 
-                                 batch_size=200, num_iterations_per_epoch=400,
-                                 normalization_func=linear_normalization_cosmos, use_only_isolated=False)
-
-validation_generator = COSMOSsequence(validation_path, 'blended_gal_stamps', 'isolated_gal_stamps', 
-                                 batch_size=200, num_iterations_per_epoch=100, 
-                                 normalization_func=linear_normalization_cosmos, use_only_isolated=False)
-
-
+train_generator.num_iterations_per_epoch=400
+train_generator.use_only_isolated=False
+validation_generator.num_iterations_per_epoch=100
+validation_generator.use_only_isolated=False
 ######## Define all used callbacks
 checkpointer_deblender_loss = tf.keras.callbacks.ModelCheckpoint(filepath=path_weights + "deblender/" + 'weights_isolated.{epoch:02d}-{val_loss:.2f}.ckpt', monitor='val_loss', verbose=1, save_best_only=True,save_weights_only=True, mode='min', period=1)
 # now train the model
-f_net.train_vae(train_generator, validation_generator, train_decoder = False, callbacks=[checkpointer_deblender_loss], epochs=deblender_epochs, path_weights=path_weights)
+f_net.train_vae(train_generator, validation_generator, train_decoder = False, callbacks=[checkpointer_deblender_loss], epochs=deblender_epochs)
