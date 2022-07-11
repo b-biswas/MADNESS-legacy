79c1f3379fd1f671a2223bf09f024f3d1de5fc9c
b-biswas <bbiswas.dgp@gmail.com>
2022-07-11 11:44:20 +0200

    added setup

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+    "matplotlib",
+    "numpy",
+    "pandas",
+    "tensorflow==2.3.0",
+    "tensorflow-probability==0.11.0",
+    "scikit-image",
+    "sep",
+]
+
+setup(
+    name="maddeb",
+    version="0.0.1",
+    author="Biswajit Biswas",
+    author_email="biswas@apc.in2p3.fr",
+    # maintainer="Biswajit Biswas",
+    # maintainer_email="biswajit.biswas@apc.in2p3.fr",
+    description="Galaxy deblender from variational autoencoders",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/b-biswas/MADNESS",
+    include_package_data=True,
+    packages=["maddeb"],
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    package_data={"maddeb": ["data/*"]},
+)
