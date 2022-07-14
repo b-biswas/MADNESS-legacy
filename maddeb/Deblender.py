3e27e59a0751ccde2d6ecf0683d5c7781b92d589
b-biswas <bbiswas.dgp@gmail.com>
2022-07-14 02:39:45 +0200

    distance to center bug fix

--- maddeb/Deblender.py
+++ maddeb/Deblender.py
@@ -326,7 +326,7 @@ class Deblend:
             m, n, b = np.shape(self.postage_stamp)
         else:
             b, m, n = np.shape(self.postage_stamp)
-        distances_to_center = list(np.array(self.detected_positions) - int((m - 1) / 2))
+        distances_to_center = list(np.array(self.detected_positions) - int(m / 2))
         cutouts = extract_cutouts(
             self.postage_stamp,
             distances_to_center,
@@ -383,9 +383,7 @@ class Deblend:
         else:
             # z = tf.Variable(name="z", initial_value=tf.random_normal_initializer(mean=0, stddev=1)(shape=[self.num_components, self.latent_dim], dtype=tf.float32))
             # use the encoder to find a good starting point.
-            distances_to_center = list(
-                np.array(self.detected_positions) - int((m - 1) / 2)
-            )
+            distances_to_center = list(np.array(self.detected_positions) - int(m / 2))
             cutouts = extract_cutouts(
                 self.postage_stamp,
                 distances_to_center,
