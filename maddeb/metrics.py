dc5bbc0c3b69ea607815d71039c0ab0f1fcd274b
b-biswas <bbiswas.dgp@gmail.com>
2022-09-29 14:58:05 +0200

    simplify metrics interface

--- maddeb/metrics.py
+++ maddeb/metrics.py
@@ -1,7 +1,8 @@
 import numpy as np
 import sep
 import skimage
-
+from numba import jit
+import pandas as pd
 
 def compute_reconstruction_metrics(predicted_images, ground_truth, channel_last=True):
     """
@@ -49,94 +50,117 @@ def compute_reconstruction_metrics(predicted_images, ground_truth, channel_last=
 def compute_pixel_covariance_and_fluxes(
     predicted_galaxies, simulated_galaxies, field_image
 ):
-    pixel_covriances = []
-    actual_fluxes = []
-    predicted_fluxes = []
+    results = {}
+
+    for band in ['u', 'g', 'r', 'i', 'z', 'y']:
+        for property in ["_covariance", "_actual_flux", "_predicted_flux"]:
+            results[band+property] = []
+            results[band+property] = []
+            results[band+property] = []
 
     for gal_num in range(len(predicted_galaxies)):
-        (
-            pixel_covriance,
-            actual_flux,
-            predicted_flux,
-        ) = compute_pixel_covariance_and_flux(
-            predicted_galaxy=predicted_galaxies[gal_num],
-            simulated_galaxy=simulated_galaxies[gal_num],
-            field_image=field_image,
-        )
+        # (
+        #     pixel_covriance,
+        #     actual_flux,
+        #     predicted_flux,
+        # ) = compute_pixel_covariance_and_flux(
+        #     predicted_galaxy=predicted_galaxies[gal_num],
+        #     simulated_galaxy=simulated_galaxies[gal_num],
+        #     field_image=field_image,
+        # )
 
-        pixel_covriances.append(pixel_covriance)
-        actual_fluxes.append(actual_flux)
-        predicted_fluxes.append(predicted_flux)
+        predicted_galaxy=predicted_galaxies[gal_num]
+        simulated_galaxy=simulated_galaxies[gal_num]
 
-    return (
-        np.array(pixel_covriances),
-        np.array(actual_fluxes),
-        np.array(predicted_fluxes),
-    )
+        for band_number, band in enumerate(['u', 'g', 'r', 'i', 'z', 'y']):
 
+            sig = sep.Background(field_image[band_number]).globalrms
+            # print(sig)
+            # print(sig[band_number])
+            mask1 = simulated_galaxy[band_number] > 0 * sig
+            mask2 = predicted_galaxy[band_number] > 0 * sig
+            mask = np.logical_or(mask1, mask2)
+            #             fig, ax = plt.subplots(1, 2)
+            #             plt.subplot(1,2,1)
+            #             plt.imshow(cutout_galaxy[:, :, band_number])
+            #             plt.subplot(1, 2, 2)
+            #             plt.imshow(madness_predictions[blend_number][galaxy_number][band_number])
+            #             plt.show()
+            band_actual_flux, band_predicted_flux, pixel_covariance = convariance_and_flux_helper(predicted_galaxy[band_number], simulated_galaxy[band_number], sig)
 
-def compute_pixel_covariance_and_flux(predicted_galaxy, simulated_galaxy, field_image):
+            results[band + "_actual_flux"].append(band_actual_flux)
+            results[band + "_predicted_flux"].append(band_predicted_flux)
+            results[band + "_covariance"].append(pixel_covariance)
+
+    return pd.DataFrame(results)
+
+@jit
+def convariance_and_flux_helper(predicted_band_galaxy, simulated_band_galaxy, sig):
     """
-    accepts only channel first"""
-    ground_truth_pixels = []
-    predicted_pixels = []
-    sig = []
+    only one band
+    """
+    mask1 = simulated_band_galaxy > 0 * sig
+    mask2 = predicted_band_galaxy > 0 * sig
+    mask = np.logical_or(mask1, mask2)
+    #             fig, ax = plt.subplots(1, 2)
+    #             plt.subplot(1,2,1)
+    #             plt.imshow(cutout_galaxy[:, :, band_number])
+    #             plt.subplot(1, 2, 2)
+    #             plt.imshow(madness_predictions[blend_number][galaxy_number][band_number])
+    #             plt.show()
+    ground_truth_pixels = simulated_band_galaxy[mask].flatten()
+    predicted_pixels = predicted_band_galaxy[mask].flatten()
 
-    actual_flux = []
-    predicted_flux = []
-
-    for band_number in range(6):
-        sig.append(sep.Background(field_image[band_number]).globalrms)
-        # print(sig)
-        # print(sig[band_number])
-        mask1 = simulated_galaxy[band_number] > 0 * sig[band_number]
-        mask2 = predicted_galaxy[band_number] > 0 * sig[band_number]
-        mask = np.logical_or(mask1, mask2)
-        #             fig, ax = plt.subplots(1, 2)
-        #             plt.subplot(1,2,1)
-        #             plt.imshow(cutout_galaxy[:, :, band_number])
-        #             plt.subplot(1, 2, 2)
-        #             plt.imshow(madness_predictions[blend_number][galaxy_number][band_number])
-        #             plt.show()
-        ground_truth_pixels.extend(simulated_galaxy[band_number][mask])
-        predicted_pixels.extend(predicted_galaxy[band_number][mask])
-
-        actual_flux.append(np.sum(simulated_galaxy[band_number][mask1]))
-        predicted_flux.append(np.sum(predicted_galaxy[band_number][mask2]))
+    band_actual_flux = np.sum(simulated_band_galaxy[mask1])
+    band_predicted_flux = np.sum(predicted_band_galaxy[mask2])
 
     pixel_covariance = np.sum(np.multiply(predicted_pixels, ground_truth_pixels)) / (
         np.sqrt(np.sum(np.square(predicted_pixels)))
         * np.sqrt(np.sum(np.square(ground_truth_pixels)))
     )
 
-    return pixel_covariance, actual_flux, predicted_flux
+    return band_actual_flux, band_predicted_flux, pixel_covariance
 
 
-def compute_apperture_photomery(residual_field, predictions, xpos, ypos, bkg_rms):
+def compute_blendedness(blended_pixels, isolated_pixels):
 
-    gal_fluxes = []
-    gal_fluxerrs = []
-    gal_flags = []
+    predicted_pixels = np.flatten(predicted_pixels)
+    blended_pixels = np.flatten(isolated_pixels)
 
-    for i in range(len(xpos)):
+    blendedness = 1 - np.sum(np.multiply(isolated_pixels, isolated_pixels))/np.sum(np.multiply(blended_pixels, isolated_pixels))
+
+    return blendedness
+
+
+def compute_apperture_photometry(residual_field, predictions, xpos, ypos, bkg_rms):
+
+    results = {}
+    for band in ['u', 'g', 'r', 'i', 'z', 'y']:
+        for column in ["_flux", "_fluxerrs", "_flags"]:
+            results[band + column] = []
+
+    for galaxy_num in range(len(xpos)):
 
         # actual galaxy
+        if predictions is None:
+            galaxy = residual_field
+        else:
+            galaxy = residual_field + predictions[galaxy_num]
 
-        galaxy = residual_field + predictions[i]
         galaxy = galaxy.copy(order="C")
 
-        for band in range(6):
+        for band_num, band in enumerate(['u', 'g', 'r', 'i', 'z', 'y']):
 
             flux, fluxerr, flag = sep.sum_circle(
-                galaxy[band],
-                [xpos[i]],
-                [ypos[i]],
+                galaxy[band_num],
+                [xpos[galaxy_num]],
+                [ypos[galaxy_num]],
                 3.0,
-                err=bkg_rms[band],
+                err=bkg_rms[band_num],
             )
 
-            gal_fluxes.extend(flux)
-            gal_fluxerrs.extend(fluxerr)
-            gal_flags.extend(flag)
+            results[band + "_flux"].extend(flux)
+            results[band + "_fluxerrs"].extend(fluxerr)
+            results[band + "_flags"].extend(flag)
 
-    return np.array(gal_fluxes), np.array(gal_fluxerrs), np.array(gal_flags)
+    return pd.DataFrame(results)
