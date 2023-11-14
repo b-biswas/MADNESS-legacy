e39b4234048ebb1cf44c2efa6d385871762308d0
b-biswas <bbiswas.dgp@gmail.com>
2023-11-14 11:36:40 +0100

    add configs

---
+++ btksims/btksims/utils.py
@@ -0,0 +1,15 @@
+import os 
+
+def get_btksims_config_path():
+    """Fetch path to maddeb config yaml file.
+
+    Returns
+    -------
+    data_dir: str
+        path to data folder
+
+    """
+    curdir = os.path.dirname(os.path.abspath(__file__))
+    get_btksims_config_path = os.path.join(curdir, "btksims_config.yaml")
+
+    return get_btksims_config_path
