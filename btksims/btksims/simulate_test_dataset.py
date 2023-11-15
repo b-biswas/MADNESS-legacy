ff3a97b06469e9913c05dbbcb34504c0c2ba7438
b-biswas <bbiswas.dgp@gmail.com>
2023-11-15 10:44:03 +0100

    sims config

--- btksims/btksims/simulate_test_dataset.py
+++ btksims/btksims/simulate_test_dataset.py
@@ -16,59 +16,49 @@ logging.basicConfig(format="%(message)s", level=logging.INFO)
 
 LOG = logging.getLogger(__name__)
 
-stamp_size = 41
 density = sys.argv[1]
 
 if density not in ["high", "low"]:
     raise ValueError("The first arguemnt should be either high or low")
 
-if density == "high":
-    min_number = 12
-    max_number = 20
-else:
-    min_number = 8
-    max_number = 15
-
 with open(get_btksims_config_path()) as f:
     btksims_config = yaml.safe_load(f)
 
 COSMOS_CATALOG_PATHS = btksims_config["CAT_PATH"]
-
 simulation_path = btksims_config["TEST_DATA_SAVE_PATH"]
+if density == "high":
+    sim_config = btksims_config["TEST_PARAMS_HIGH_DESNSITY"]
+else:
+    sim_config = btksims_config["TEST_PARAMS_LOW_DESNSITY"]
 
-
-batch_size = 20
-maxshift = 15
-num_repetations = 300
 catalog = btk.catalog.CatsimCatalog.from_file(COSMOS_CATALOG_PATHS)
-survey = btk.survey.get_surveys("LSST")
-seed = 13
+survey = btk.survey.get_surveys(sim_config["survey_name"])
 
-index_range = [200000, len(catalog.table)]
+index_range = [sim_config["index_start"], len(catalog.table)]
 sampling_function = CustomSampling(
     index_range=index_range,
-    min_number=min_number,
-    max_number=max_number,
-    maxshift=maxshift,
-    stamp_size=stamp_size,
-    seed=seed,
-    unique=False,
+    min_number=sim_config["min_number"],
+    max_number=sim_config["max_number"],
+    maxshift=sim_config["maxshift"],
+    stamp_size=sim_config["stamp_size"],
+    seed=sim_config["btk_seed"],
+    unique=sim_config["unique_galaxies"].lower() == "true",
 )
 
 draw_generator = btk.draw_blends.CatsimGenerator(
     catalog,
     sampling_function,
     survey,
-    batch_size=batch_size,
-    stamp_size=stamp_size,
+    batch_size=sim_config["batch_size"],
+    stamp_size=sim_config["stamp_size"],
     cpus=1,
     add_noise="all",
     verbose=False,
-    seed=seed,
+    seed=sim_config["btk_seed"],
     augment_data=False,
 )
 
-for file_num in range(num_repetations):
+for file_num in range(sim_config["num_repetations"]):
     print("Processing file " + str(file_num))
     blend = next(draw_generator)
 
